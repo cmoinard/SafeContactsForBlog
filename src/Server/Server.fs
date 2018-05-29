70ab7bd133e264fb04679e72195f78acac02291f
Christophe Moinard <cmoinard@mail.fr>
2018-05-29 13:31:40 +0200

    Generate persons in Server

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -2,6 +2,9 @@ open System.IO
 
 open Microsoft.AspNetCore.Builder
 open Saturn
+
+open Bogus
+
 open Shared
 
 open Fable.Remoting.Giraffe
@@ -9,22 +12,28 @@ open Fable.Remoting.Giraffe
 let publicPath = Path.GetFullPath "../Client/public"
 let port = 8085us
 
-let personRepository : PersonRepository =
-    let persons = [
-        { id = 1
-          firstName = "Jean"
-          lastName = "Durand"
-          address =
-            { number = 5
-              street = "rue de la baguette"
-              postalCode = "44000"
-              city = "Nantes"
-            }
-        } ]
+let personRepository =
+    let generator =
+        Faker<Person>("fr")
+            .CustomInstantiator(fun f ->
+                { id = f.Random.Number()
+                  firstName = f.Name.FirstName()
+                  lastName = f.Name.LastName()
+                  address = 
+                    { number = f.Random.Number(0, 100)
+                      street = f.Address.StreetName()
+                      postalCode = f.Address.ZipCode()
+                      city = f.Address.City()
+                    }
+                })
+
+    let persons =
+        generator.Generate(10)
+        |> List.ofSeq
 
     {
         getAll = fun () -> async {
-            do! Async.Sleep 500
+            do! Async.Sleep 2000
             return persons
         }
     }
